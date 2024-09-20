6114902c151f70bcd2df3a4b03f053442d345fea
Sun Mingfu <mingfu.sun1@epfedu.fr>
2024-09-20 12:44:34 +0200

    Exo

--- Calculator/src/main/java/com/mycompany/calculator/Calculator.java
+++ Calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -14,11 +14,12 @@ public class Calculator {
 
     public static void main(String[] args) {
         
-        int resultat = 04;
+        int resultat = 0;
         
         
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter the operator:\n1) add\n2) substract\n3) multiply\n4) divide\n5) modulo");
+        System.out.println();
         int operateur = sc.nextInt();
         System.out.println("Please enter the first number:");
         int operande1 = sc.nextInt();
@@ -27,21 +28,23 @@ public class Calculator {
         
         if (operateur == 1){
             resultat = operande1 + operande2;
+            System.out.println("The result is : " + resultat);
         }
         else if (operateur == 2){
             resultat = operande1 - operande2;
+            System.out.println("The result is : " + resultat);
         }
         else if (operateur == 3){
             resultat = operande1 * operande2;
+            System.out.println("The result is : " + resultat);
         }
         else if (operateur == 4){
             resultat = operande1 / operande2;
+            System.out.println("The result is : " + resultat);
         }
         else {
-            System.out.println("Please enter a value from 1 to 5");
+            System.out.println("Please enter a value of the operatorfrom 1 to 5");
         }
             
-        
-        System.out.println("The result is : " + resultat);
     }
 }
