2a2370bb169d5558055b09f89506a0446f70d5aa
Sun Mingfu <mingfu.sun1@epfedu.fr>
2024-09-20 12:32:27 +0200

    Exo

--- Exo2/src/main/java/com/mycompany/exo2/Exo2.java
+++ Exo2/src/main/java/com/mycompany/exo2/Exo2.java
@@ -1,5 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * TP1 Exercice 1
+ * 17/09/2024
  */
 
 package com.mycompany.exo2;
@@ -14,11 +15,26 @@ public class Exo2 {
 
     public static void main(String[] args) {
         
-        int nb;
+        //Declaration des variables
+        int nb; // nombre dentiers a additionner
+        int result; // resultat
+        int ind; //indice
+
+        result=0;
         //nb=5;
         Scanner sc = new Scanner(System.in);
         System.out.println("\n Entrer le nombre :");
         nb=sc.nextInt(); // On demande a sc de donner le prochain entier
-        System.out.println(nb);
+
+        // Addition des nb premiers entiers
+        ind=1;
+        
+        while (ind <= nb) {
+            result=result+ind; 
+        // Affichage du resultat
+        ind = ind + 1;
+        }
+        System.out.println();
+        System.out.println("La somme des "+ nb + "entiers est: "+result); 
     }
 }
